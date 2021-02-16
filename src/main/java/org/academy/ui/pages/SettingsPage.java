2560e5d09d792b3689b1c912f4f4be8745de4963
Denys Heraskin <dheraskin@gmail.com>
2021-02-16 19:26:16 +0200

    add SettingsTest

--- src/main/java/org/academy/ui/pages/SettingsPage.java
+++ src/main/java/org/academy/ui/pages/SettingsPage.java
@@ -1,15 +1,30 @@
 package org.academy.ui.pages;
 
 import org.openqa.selenium.By;
+import org.academy.ui.*;
 
-import java.util.Set;
+import static org.testng.Assert.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
 
 public class SettingsPage extends AbstractPage {
 
-    private static final By email = By.xpath("");
+    private static final By email = By.xpath("//input[@id='email']");
 
     public SettingsPage() { super(); }
     public SettingsPage(boolean navigate, String navigateToURL) {
         super(navigate, navigateToURL);
+    }
+
+    public String getEmail() {
+        return this.findElement(email).getAttribute("value");
+    }
+
+    public String actualEmail() {
+        return "ssacd1@mailforspam.com";
+    }
+
+    public SettingsPage checkEmail() {
+        assertEquals(getEmail(), actualEmail(), "Invalid value of email in user's settings!");
+        return this;
     }
 }
