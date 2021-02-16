8778b155382747097099315acad6dd882c1a3150
Denys Heraskin <dheraskin@gmail.com>
2021-02-16 18:18:21 +0200

    modify MainPage to add new By variables

--- src/main/java/org/academy/ui/pages/MainPage.java
+++ src/main/java/org/academy/ui/pages/MainPage.java
@@ -17,7 +17,9 @@ public class MainPage extends AbstractPage {
     private static final By addProjectBtn = By.xpath("//a[@id='sidebar-projects-add']");
     private static final By administrationLink = By.xpath("//a[@id='navigation-admin']");
     private static final By projectLink = By.xpath("//div[@class='summary-title text-ppp']//a[contains(text(),'TestProject')]");
-
+    private static final By navigationUser = By.xpath("//a[@id='navigation-user']//span[@class='caret']");
+    private static final By navigationUserSettings = By.xpath("//a[@id='navigation-user-settings']");
+    
     public MainPage clickOnMyUsername() {
         waitUntilElementIsClickable(usernameLink).click();
         return this;
