15977450a17520d9e469e56cbbfc0fe0c1b30d14
Denys Heraskin <dheraskin@gmail.com>
2021-02-22 00:55:09 +0200

    modify MainPage to make clickOnProject() independent of TR account

--- src/main/java/org/academy/ui/pages/MainPage.java
+++ src/main/java/org/academy/ui/pages/MainPage.java
@@ -17,7 +17,7 @@ public class MainPage extends AbstractPage {
     private static final By logout = By.xpath("//a[@id='navigation-user-logout']");
     private static final By addProjectBtn = By.xpath("//a[@id='sidebar-projects-add']");
     private static final By administrationLink = By.xpath("//a[@id='navigation-admin']");
-    private static final By projectLink = By.xpath("//a[contains(text(),'UI Suite Tests')]");
+    private static final By projectLink = By.xpath("//div[@class='summary-title text-ppp']//a[contains(text(),'UI Suite Tests')]");
     private static final By navigationUser = By.xpath("//a[@id='navigation-user']//span[@class='caret']");
     private static final By navigationUserSettings = By.xpath("//a[@id='navigation-user-settings']");
     private static final By project = By.xpath("//a[@href='index.php?/projects/overview/20'][contains(.,'TestProject')]");
@@ -43,7 +43,18 @@ public class MainPage extends AbstractPage {
     }
 
     public SuitePage clickOnProjectLink() {
-        waitUntilElementIsClickable(projectLink).click();
+        if (webDriver.findElement(projectLink).isDisplayed()) {
+            waitUntilElementIsClickable(projectLink).click();
+        } else {
+            System.err.println("Project 'UI Suite Tests' not found");
+            System.out.println("Creating new project...");
+            waitUntilElementIsClickable(addProjectBtn).click();
+            webDriver.findElement(By.xpath("//input[@id='name']")).sendKeys("UI Suite Tests");
+            webDriver.findElement(By.xpath("//button[@id='accept']")).click();
+            System.out.println("Project 'UI Suite Tests' created");
+            webDriver.findElement(By.xpath("//a[@id='navigation-dashboard']")).click();
+            waitUntilElementIsClickable(projectLink).click();
+        }
         return new SuitePage();
     }
 
