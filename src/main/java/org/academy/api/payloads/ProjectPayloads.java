e181985b1d505c529b9fc8e255b70f42c95d4de3
AlexFox <mobilab@MacBook-Pro.local>
2021-02-23 21:41:09 +0200

    implemented API test scenario to get all project attributes from existing project

--- src/main/java/org/academy/api/payloads/ProjectPayloads.java
+++ src/main/java/org/academy/api/payloads/ProjectPayloads.java
@@ -1,11 +1,28 @@
 package org.academy.api.payloads;
 
+import org.json.JSONObject;
+
 public class ProjectPayloads {
+    public static final String ANNOUNCEMENT = "This is the description for the project";
+    public static final boolean SHOW_ANNOUNCEMENT = true;
+    public static final boolean IS_COMPLETED = true;
+
     public String projectPayload(String projectName) {
         return String.format("{\n" +
                 "\t\"name\": \"%s\",\n" +
                 "\t\"announcement\": \"This is the description for the project\",\n" +
                 "\t\"show_announcement\": true\n" +
                 "}", projectName);
+    }
+
+    public JSONObject projectAllFieldsPayload(String projectName) {
+        JSONObject requestParams = new JSONObject();
+
+        requestParams.put("name", projectName);
+        requestParams.put("announcement", ANNOUNCEMENT);
+        requestParams.put("show_announcement", SHOW_ANNOUNCEMENT);
+        requestParams.put("is_completed", IS_COMPLETED);
+
+        return requestParams;
     }
 }
--- src/test/java/org/academy/api/ProjectTest.java
+++ src/test/java/org/academy/api/ProjectTest.java
@@ -1,8 +1,14 @@
 package org.academy.api;
 
 import org.academy.api.requests.ProjectRequests;
+import org.academy.api.requests.UserRequests;
+import org.json.JSONObject;
 import org.testng.annotations.Test;
 
+import java.util.Map;
+import java.util.Set;
+
+import static org.academy.api.payloads.ProjectPayloads.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProjectTest extends BaseTest {
@@ -14,6 +20,29 @@ public class ProjectTest extends BaseTest {
         String projectNameFromResponse =
                 String.valueOf(projectRequests.createProjectRequest(projectName).get("role"));
         assertThat(projectNameFromResponse).isEqualTo(projectName)
-                                           .as("Project name from response should be the same as " + projectName);
+                .as("Project name from response should be the same as " + projectName);
+    }
+
+    @Test
+    public void getProjectAttributesCase() {
+        Map<String, Object> attributes = projectRequests.getProjectRequest();
+        assertThat(String.valueOf(attributes.get("name"))).isEqualTo("111").as("name of project should be 111");
+        assertThat(String.valueOf(attributes.get("announcement"))).isEqualTo("Test project").as("announcement of project should be Test project");
+        assertThat(String.valueOf(attributes.get("show_announcement"))).isEqualTo("false").as("show_announcement field of project should be false");
+        assertThat(String.valueOf(attributes.get("is_completed"))).isEqualTo("false").as("is_completed field of project should be false");
+        assertThat(String.valueOf(attributes.get("completed_on"))).isEqualTo("null").as("completed_on field of project should be null");
+        assertThat(String.valueOf(attributes.get("suite_mode"))).isEqualTo("1").as("suite_mode field of project should be 1");
+        assertThat(String.valueOf(attributes.get("url"))).isEqualTo("https://ssacd2.testrail.io/index.php?/projects/overview/1");
+    }
+
+    @Test
+    public void updateProjectAttributesCase() {
+        String updated_project = "Updated project";
+        Map<String, Object> attributes = projectRequests.updateProjectRequest(updated_project);
+        assertThat(String.valueOf(attributes.get("name"))).isEqualTo(updated_project).as("name of project should be "+updated_project);
+        assertThat(String.valueOf(attributes.get("announcement"))).isEqualTo(ANNOUNCEMENT).as("announcement of project should be Test project");
+        assertThat(String.valueOf(attributes.get("show_announcement"))).isEqualTo(String.valueOf(SHOW_ANNOUNCEMENT)).as("show_announcement field of project should be false");
+        assertThat(String.valueOf(attributes.get("is_completed"))).isEqualTo(String.valueOf(IS_COMPLETED)).as("is_completed field of project should be false");
     }
 }
+
