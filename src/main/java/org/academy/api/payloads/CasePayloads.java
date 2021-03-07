f524e2909ad27420674ae9e09d54b2e252a31404
Denys Heraskin <dheraskin@gmail.com>
2021-03-07 15:04:28 +0200

    modify CasePayloads

--- src/main/java/org/academy/api/payloads/CasePayloads.java
+++ src/main/java/org/academy/api/payloads/CasePayloads.java
@@ -1,4 +1,15 @@
 package org.academy.api.payloads;
 
-public class CasePayloads {
+import lombok.extern.slf4j.Slf4j;
+import org.academy.api.models.cases.CaseModel;
+
+@Slf4j
+public class CasePayloads extends Payload{
+    public String createCasePayload(String title) {
+        CaseModel model = new CaseModel.Builder()
+                .title(title).build();
+        String payload = jsonFromObject(model);
+        log.info("payload created = \r\n" + payload);
+        return payload;
+    }
 }
